3f39dfc9122ace08d50bd70cfa8ac66015bc5f8e
Mark Wong <quik_silv@hotmail.com>
2015-11-17 01:05:51 +0100

    Ctrl+C to stop collecting data

--- LJ_counter.py
+++ LJ_counter.py
@@ -4,13 +4,23 @@
 import u3
 from datetime import datetime
 from time import sleep
-f = open('counter.log', 'w')
-d = u3.U3()
-d.configU3()
-d.getCalibrationData()
-d.configIO(TimerCounterPinOffset = 6, EnableCounter1 = True, FIOAnalog = 15)
-u3.Counter1(Reset=True)
-while(True):
-    print datetime.now(), d.getFeedback(u3.Counter1(Reset = False) )[0]
-    f.write(str(datetime.now() ) + " " +  str(d.getFeedback(u3.Counter1(Reset = False) )[0]) + "\n")
-    sleep(0.5)
+f = open('counter.log', 'a')
+def counter():
+    d = u3.U3()
+    d.configU3()
+    d.getCalibrationData()
+    d.configIO(TimerCounterPinOffset = 6, EnableCounter1 = True, NumberOfTimersEnabled = 1, FIOAnalog = 15)
+    u3.Counter1(Reset=True)
+    while(True):
+        print datetime.now(), d.getFeedback(u3.Counter1(Reset = False) )[0]
+        f.write(str(datetime.now() ) + " " +  str(d.getFeedback(u3.Counter1(Reset = False) )[0]) + "\n")
+        sleep(0.5)
+def main():
+    try:
+        counter()
+    except (KeyboardInterrupt, EOFError):
+        f.write(str(datetime.now() ) + " counter stopped.\n")
+        pass
+
+if __name__ == '__main__':
+    main()
